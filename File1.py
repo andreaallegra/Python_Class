dd304b928386dcc0b221068cb35110edf052a98b
aallegra <andrea.allegra@numerix.com>
2018-05-06 08:46:36 +0100

    end of file1
    Signed-off-by: aallegra <andrea.allegra@numerix.com>

--- File1.py
+++ File1.py
@@ -1,3 +1,66 @@
+
+
 class Employee:
-    pass
+    def __init__(self, first, last, pay):
+        self.first = first  #this is the same as writing emp_1.first = 'Corey'
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + "@employee.com"
+
+    def fullname(self):
+        # and use the same login as the first approach
+        return '{} {}'.format(self.first, self.last)
+
+
+
+# instances of Employee class
+
+# old (before the __init__ method
+#emp_1 = Employee()
+#emp_2 = Employee()
+
+# new (after the __init__ method
+# now we can pass the values that we specified in the init method,
+# excluding the instance because the instance is pass automatically
+emp_1 = Employee('Corey', 'Schafer', 50000)
+emp_2 = Employee('Test', 'User', 60000)
+
+#when these lines are read the __init__method gets called and create all the instance variable:
+# self.first = Corey...
+# and now we can delete this manual assignments below
+
+print(emp_1)
+print(emp_2)
+
+
+# emp_1 and emp_2 are Employee objects
+
+# create a instance variable (instance variable contains data that are specific to a class instance)
+
+# emp_1.first = 'Corey'
+# emp_1.first = 'Schafer'
+# emp_1.email = 'Corey.Schafer@employee.com'
+# emp_1.pay = 50000
+#
+# emp_2.first = 'Test'
+# emp_2.first = 'User'
+# emp_2.email = 'Test.User@employee.com'
+# emp_2.pay = 60000
+
+print(emp_1.email)
+
+# and now within the __init__ method I am going to see all the instance variables
+
+#idea; show full name.
+# first approach
+print('{} {}'.format(emp_1.first, emp_1.last))
+#2 second: create a class method
+print(emp_1.fullname())
+# look that we need to add the () because this is a method instead of an attribute
+# I don't need to pass the instance name because it's passed automatically
+
+# you can run the method using the class name itself
+# and here I need to explicitly the instance name as an argument because it doesn't know the instance which we want
+# to run the method with
+print(Employee.fullname(emp_1))
 
--- README.md
+++ README.md
@@ -6,7 +6,25 @@ https://www.youtube.com/watch?v=3ohzBxoFHAY
 https://www.youtube.com/watch?v=jCzT9XFZ5bw
 https://www.youtube.com/watch?v=FsAPt_9Bf3U
 
-When we talked about data and functions that are associated with a specific class we call them attribuites and methods 
+When we talked about data and functions that are associated with a specific class we call them attributes and methods
 method: is a function associated with a class
 
 now let's create a simple class
+a class is a blueprint for creating instances
+
+instance variables and class variables are different
+
+instance variables contains data that are specific for a class instance 
+
+now let's create a __init__ method within the class (the init method is the constructor)
+when you create methods within a class they receive the instance as the first argument automatically
+and we should call the instance self
+
+names, pay, e-mail are attributes of the class
+now if we want to perform some actions we need to add some methods to the class
+idea: show full name of the employee: how? asking a class method to do so
+remember to always pass the instance name (self) as a first argument of the method definition:
+this what happens if you don't pass self:fullname() takes 0 positional arguments but 1 was given
+because the instance name is pass automatically
+
+next: difference between class variables and instance variables
